aab1fd3b1fc8402a58c4166a916cdd6b16f0396e
Sergey Lapin <slapinid@gmail.com>
2018-09-30 00:38:48 +0300

    Detour module for Godot engine navigation support

---
+++ modules/detour/obstacle.cpp
@@ -0,0 +1,15 @@
+#include "obstacle.h"
+#include "detour.h"
+DetourNavigationObstacle::DetourNavigationObstacle() :
+		radius(5.0f),
+		height(5.0f),
+		mesh(0),
+		id(0) {
+}
+
+DetourNavigationObstacle::~DetourNavigationObstacle() {
+	if (mesh && id > 0)
+		mesh->remove_obstacle(id);
+}
+void DetourNavigationObstacle::_bind_methods() {
+}
